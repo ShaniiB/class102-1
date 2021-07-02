099d727f25bdf3c39a9ad302f4bcd6330308a443
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-07-02 15:34:04 -0700

    workplz

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-let answer = prompt('Web design an art form ?');
-if (answer == Yes) 
-  alert( "Correct!" );
-  else
-  alert( "EEEE, WRONG!" );
-  
+  //let answer = prompt('Web design as an art form ?');
+  //if (answer == Yes) 
+    alert( "Correct!" );
+    else ( alert ("EEEE, Wrong") )
