1695fd52520b7c580db692101b219ec54a3593ae
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-07-04 13:17:05 -0700

    testin

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let answer = prompt('Do you like Space?');
-console.log (answer);
+console.log(answer);
 if (answer === "Yes" || "yes"); {
     alert('Sweet!');   
-let prompt = ('Do you like Llamas?');
+let answwer = prompt('Do you like Llamas?');
+console.log(answer)
     alert('You better!');
     alert('LLAMAS IN SPAAAAACEEEE')} 
