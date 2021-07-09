65ba93a4b54b4c374747200410cb915eef5f5dd3
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-07-08 20:52:30 -0700

    fixin'

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let question; {
   alert("Hiya!\nThanks for dropping by!");
   alert("Hope you are having a good day today!");
-  let r = confirm("Press Ok to proceed to my page!"); 
-  if (r == true) {
-    question = alert('Sweet!\nEnjoy yourself');
-  } else {
-       alert('Wow, rude!');
-}
-
- var moon = prompt("Do you like moons?\nIf so how many do you want?");
-   for(let i = 0; i < moon; i++){
-  document.write("<img src ='https://i.imgur.com/MrSdk27.png?1'>");
-  console.log(i);
+function askQuestion(){
+  var answerfound = false;
+  while(answerfound ==false){
+      var answer = prompt("How many planets are there?");
+      if(answer == "8"){
+          alert("Nerd. ◕‿↼");
+          answerfound = true;
+      }
+      else{
+          alert("ʘ‿ʘ NOPE.");
+      }
+  
   }
+
+} 
+askQuestion();
+
+var moon = prompt("Do you like moons?\nIf so how many do you want?");
+for(let i = 0; i < moon; i++){
+document.write("<img src ='https://i.imgur.com/MrSdk27.png?1'>");
+console.log(i);
+}
 }
